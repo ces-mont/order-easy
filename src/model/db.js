03eeac53076f6426db4e2a494c88ea0bfc8799fd
ces-mont <cmontalvan@frba.utn.edu.ar>
2023-11-26 16:00:19 -0300

    A db de clever-cloud 2

--- src/model/db.js
+++ src/model/db.js
@@ -1,7 +1,7 @@
 const {Sequelize, DataTypes} = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-    host: process.env.DB_HOSTNAME,
+const sequelize = new Sequelize(process.env.DB_NAME2,process.env.DB_USER2,process.env.DB_PASS2,{
+    host: process.env.DB_HOSTNAME2,
     dialect: 'mysql',
     timezone:'America/Argentina/Buenos_Aires',
     dialectOptions:{
